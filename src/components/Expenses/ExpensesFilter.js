5efac1b5edab356655809ee8552c2fc08c5ba5f7
Anvitha Swarangi <aswaran1@in.ibm.com>
2021-06-14 12:08:45 +0530

    fixed names

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,26 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './ExpensesFilter.css';
 
 const ExpensesFilter = (props) => {
 
-  const [enteredYear, setEnteredYear] = useState('');
-
-  const yearFilterHandler = (event) => {
-    setEnteredYear(event.target.value);
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
   };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-    props.onSaveFilter(enteredYear);
-    setEnteredYear('');
-  };
-
   return (
-    <div className='expenses-filter' onSubmit={submitHandler}>
+    <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select onChange={yearFilterHandler}>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
